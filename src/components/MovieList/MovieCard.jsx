a8789c78fd7c31c1e94d73610c4300f785b0a698
Pranav Bhartiya <bhartiya.pranav@gmail.com>
2024-09-18 14:55:06 -0700

    new features added

--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 import './MovieCard.css';
 import Star from '../../assets/star.png';
 
-const MovieCard = () => {
+const MovieCard = ({movie}) => {
   return (
-    <a href="" className="movie_card">
-        <img src="https://images.squarespace-cdn.com/content/v1/5a7f41ad8fd4d236a4ad76d0/1669842753281-3T90U1EY5HUNCG43XERJ/A2_Poster_DC_v80_PAYOFF_221029_12trimHD.jpg" alt="movie poster" className="movie_poster" />
+    <a href={`https://www.themoviedb.org/movie/${movie.id}`} target='_blank' className="movie_card">
+        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="movie poster" className="movie_poster" />
 
         <div className="movie_details">
-            <h3 className="movie_details_heading">Movie Name</h3>
+            <h3 className="movie_details_heading">{movie.original_title}</h3>
             <div className="movie_date_rate">
-                <p>10-20-2020</p>
-                <p>8.0 <img src={Star} alt="rating icon" className='card_emoji' /></p>
+                <p>{movie.release_date}</p>
+                <p>{movie.vote_average} <img src={Star} alt="rating icon" className='card_emoji' /></p>
             </div>
             <p className="movie_description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod iste dignissimos reiciendis temporibus nobis aspernatur!
+                {movie.overview.slice(0, 100) + "..."}
             </p>
         </div>
     </a>
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -5,19 +5,47 @@ import Fire from '../../assets/fire.png';
 import Star from '../../assets/glowing-star.png';
 import Party from '../../assets/partying-face.png';
 import MovieCard from './MovieCard';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import FilterGroup from './FilterGroup';
+import API_KEY from '../../../apikey';
 
 const MovieList = () => {
+    const [movies, setMovies] = useState([]);
+    const [filterMovies, setFilterMovies] = useState([]);
+    const [minRating, setMinRating] = useState(0);
+
+    useEffect(() => {
+        fetchMovies();
+    }, []);
+
+    const fetchMovies = async () => {
+        const apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key=" + API_KEY; // Concatenate API key with the URL
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        setMovies(data.results);
+        setFilterMovies(data.results);
+    };
+
+    const handleFilter = rate => {
+        if(rate === minRating) {
+            setMinRating(0);
+            setFilterMovies(movies);
+        } else {
+            setMinRating(rate);
+
+            const filtered = movies.filter(movie => movie.vote_average >= rate);
+            setFilterMovies(filtered);
+        }
+    };
+
   return (
     <section className="movie_list">
         <header className="movie_list_header">
             <h2 className="movie_list_heading">Popular <img src={Fire} alt="fire emoji" className='navbar_emoji' /></h2>
 
             <div className="movie_list_fs">
-                <ul className="movie_filter">
-                    <li className="movie_filter_item active">8+ Star</li>
-                    <li className="movie_filter_item">7+ Star</li>
-                    <li className="movie_filter_item">6+ Star</li>
-                </ul>
+                <FilterGroup minRating={minRating} onRatingClick={handleFilter} ratings={[8,7,6]} />
 
                 <select name="" id="" className="movie_sorting">
                     <option value="">SortBy</option>
@@ -32,7 +60,9 @@ const MovieList = () => {
         </header>
 
         <div className="movie_cards">
-            <MovieCard />
+            {
+                filterMovies.map((movie) => (<MovieCard key={movie.id} movie={movie} />))
+            }
         </div>
     </section>
   )
